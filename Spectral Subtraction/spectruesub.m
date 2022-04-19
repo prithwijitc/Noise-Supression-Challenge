6fb887f5d3cc8c825e5a941fb7b2240bf7972a36
Aanish Nair <52620439+nairaanish@users.noreply.github.com>
2022-04-18 20:54:48 -0400

    updated comments to english

--- Spectral Subtraction/spectruesub.m
+++ Spectral Subtraction/spectruesub.m
@@ -1,42 +1,43 @@
 function [spectruesub_enspeech] = spectruesub(testsignal)
-% 谱减法函数
-% testsignal为带噪语音信号
-% spectruesub_enspeech为谱减法处理后信号
+% Spectral Subtraction Function
+% testsignal is a noisy speech signal
+% spectruesub_enspeech is the signal after spectral subtraction
 testsignal=testsignal';
-%-------------------------------参数定义---------------------------------
-frame_len=256; %帧长
-step_len=0.5*frame_len; %分帧时的步长，相当于重叠50%
+%------------------------------PARAMETER DEFINITION----------------------------------
+frame_len=256; % frame length
+step_len=0.5*frame_len; % The step size when dividing the frame, which is equivalent to 50% overlap
 wav_length=length(testsignal);
 R = step_len;
 L = frame_len; 
 f = (wav_length-mod(wav_length,frame_len))/frame_len;
-k = 2*f-1; % 帧数
-h = sqrt(1/101.3434)*hamming(256)'; % 汉宁窗乘以系数的原因是使其复合条件要求；
-testsignal = testsignal(1:f*L);  % 带噪语音与纯净语音长度对齐
+k = 2*f-1; % frame number
+h = sqrt(1/101.3434)*hamming(256)'; % The reason why the Hanning window is multiplied by 
+% the coefficient is to make it compound the conditional requirements
+testsignal = testsignal(1:f*L);  % Noisy speech is length-aligned with clean speech
 % signal= signal(1:f*L);
-win = zeros(1,f*L); % 设定初始值；
+win = zeros(1,f*L); % set initial value
 spectruesub_enspeech = zeros(1,f*L);       
-%-------------------------------分帧-------------------------------------
+%--------------------------------FRAMING------------------------------------
 for r = 1:k 
-    y = testsignal(1+(r-1)*R:L+(r-1)*R); % 对带噪语音帧间重叠一半取值；
-    y = y.*h; % 对取得的每一帧都加窗处理；
-    w = fft(y); % 对每一帧都作傅里叶变换；
-    Y(1+(r-1)*L:r*L) = w(1:L); % 把傅里叶变换值放在Y中；
+    y = testsignal(1+(r-1)*R:L+(r-1)*R); % Value for half overlap between noisy speech frames
+    y = y.*h; % Windowing each frame obtained 
+    w = fft(y); % Fourier transform for each frame
+    Y(1+(r-1)*L:r*L) = w(1:L); % put the Fourier transform value in Y
 end
-%-------------------------------估计噪声-----------------------------------
-   NOISE= stationary_noise_evaluate(Y,L,k); %噪声最小值跟踪算法
-%-------------------------------谱减法-------------------------------------
+%---------------------------------ESTIMATED NOISE---------------------------------
+   NOISE= stationary_noise_evaluate(Y,L,k); % Noise Minimum Tracking Algorithm
+%-------------------------------脝脳录玫路篓-------------------------------------
 for     t = 1:k     
          X = abs(Y).^2;   
-         S = X(1+(t-1)*L:t*L)-NOISE(1+(t-1)*L:t*L); % 含噪语音功率谱减去噪声功率谱；
+         S = X(1+(t-1)*L:t*L)-NOISE(1+(t-1)*L:t*L); % Noisy speech power spectrum minus noise power spectrum
          S = sqrt(S);
-         A = Y(1+(t-1)*L:t*L)./abs(Y(1+(t-1)*L:t*L)); % 带噪于语音的相位；
-         S = S.*A; % 因为人耳对相位的感觉不明显，所以恢复时用的是带噪语音的相位信息；
+         A = Y(1+(t-1)*L:t*L)./abs(Y(1+(t-1)*L:t*L)); % phase with noise in speech
+         S = S.*A; % Because the human ear does not perceive the phase clearly, the phase information of the noisy speech is used for recovery.
          s = ifft(S);   
-         s = real(s); % 取实部；
-         spectruesub_enspeech(1+(t-1)*L/2:L+(t-1)*L/2) = spectruesub_enspeech(1+(t-1)*L/2:L+(t-1)*L/2)+s; % 在实域叠接相加；
-         win(1+(t-1)*L/2:L+(t-1)*L/2) = win(1+(t-1)*L/2:L+(t-1)*L/2)+h; % 窗的叠接相加；
+         s = real(s); % take the real part
+         spectruesub_enspeech(1+(t-1)*L/2:L+(t-1)*L/2) = spectruesub_enspeech(1+(t-1)*L/2:L+(t-1)*L/2)+s; % Concatenate and Add in the Real Domain
+         win(1+(t-1)*L/2:L+(t-1)*L/2) = win(1+(t-1)*L/2:L+(t-1)*L/2)+h; % window stacking
 end
-spectruesub_enspeech = spectruesub_enspeech./win; % 去除加窗引起的增益得到增强的语音；
+spectruesub_enspeech = spectruesub_enspeech./win; % Remove the gain-enhanced speech caused by windowing
 spectruesub_enspeech=spectruesub_enspeech';
 end
