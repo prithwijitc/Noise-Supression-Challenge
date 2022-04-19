c2bc7cc855c114e9dfbce62521f8a54a757a0aea
Aanish Nair <52620439+nairaanish@users.noreply.github.com>
2022-04-18 20:54:21 -0400

    Update main.m

--- Spectral Subtraction/main.m
+++ Spectral Subtraction/main.m
@@ -1,3 +1,14 @@
+clc; clear all; close all;
+
 [y, fs]=audioread('$ABSOLUTE_PATH_TO_NOISY_WAV$');
 [spectruesub_enspeech] = spectruesub(y);
 audiowrite('$ABSOLUTE_PATH_TO_STORE_FILTERED_WAV$',spectruesub_enspeech,fs);
+
+
+fprintf("Playing Noisy Speech\n");
+clean_speech = audioplayer(y(1:100000), fs);
+playblocking(clean_speech);
+
+fprintf("Playing Reconstructed Speech\n");
+reconstructed_speech = audioplayer(spectruesub_enspeech(1:100000), fs);
+playblocking(reconstructed_speech);
